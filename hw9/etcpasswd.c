2f6b70e338688cba5ec803d4050a88ce2e7a753e
ggd <steve04282009@gmail.com>
2026-02-19 23:49:35 +0800

    Finish hw9, update gitignore

--- hw9/etcpasswd.c
+++ hw9/etcpasswd.c
@@ -3,7 +3,7 @@
 #include <string.h>
 #define BUFFER_SIZE 4096
 char readline[BUFFER_SIZE];
-typedef struct Passwd{
+typedef struct {
     char* username;
     char* password;
     int uid;
@@ -11,27 +11,50 @@ typedef struct Passwd{
     char* gecos;
     char* directory;
     char* shell;
+} Passwd;
+void print_passwd_struct(const Passwd *p) {
+	if (p == NULL) return;
 
-    int username_len;
-    int password_len;
-    int gecos_len;
-    int directory_len;
-    int shell_len;
-};
-
-void split_tostruct(){
-    char *token;
-    token = strtok(readline, ":");
-    while (strtok(NULL, ":")){
-        
-    }
-
+	printf("[User: %-15s] UID: %-5d GID: %-5d Shell: %s\n", 
+		p->username ? p->username : "N/A", 
+		p->uid, 
+		p->gid, 
+		p->shell ? p->shell : "N/A"
+	);
+	printf("  |- Directory: %s\n", p->directory ? p->directory : "N/A");
+	printf("  |- Gecos:     %s\n", p->gecos ? p->gecos : "N/A");
+	printf("------------------------------------------------------------------\n");
 }
-void read_passwd(){
+void split_tostruct(Passwd *passwd){
+    char *running = readline;
+    char *token;
+    // 1. username
+    passwd->username = strsep(&running, ":");
+
+    // 2. password (遇到 :: 會回傳空字串 "")
+    passwd->password = strsep(&running, ":");
+
+    // 3. uid
+    token = strsep(&running, ":");
+    passwd->uid = token ? atoi(token) : 0;
+
+    // 4. gid
+    token = strsep(&running, ":");
+    passwd->gid = token ? atoi(token) : 0;
+
+    passwd->gecos = strsep(&running, ":");
+
+    passwd->directory = strsep(&running, ":");
+
+    passwd->shell = strsep(&running, ":");
+    int i;
+    for (i = 0; passwd->shell[i] != '\n'; ++i);
+    passwd->shell[i] = '\0';
+     
+}
+int read_passwd(char *username, Passwd *passwd){
     FILE *fp = fopen("/etc/passwd", "r");
     long filesize;
-    Passwd passwd[1000];
-    int linecount = 0;
 
     // 0. check file
     if (fp == NULL) {
@@ -46,15 +69,14 @@ void read_passwd(){
     // 2. read (move)
     while (ftell(fp) < filesize){
         fgets(readline, BUFFER_SIZE, fp); // read 4096 in once
-        split_tostruct();
+        split_tostruct(passwd);
+        //print_passwd_struct(&passwd);
+        if (strcmp(passwd->username, username)== 0) 
+            return 1;
     }
 
     fclose(fp);
+    return 0;
 }
 
-
-int main(){
-    read_passwd();
-
-}
 
--- hw9/mylogin.c
+++ hw9/mylogin.c
@@ -7,29 +7,96 @@
 */
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include <sys/wait.h> // For wait
-       
+#include "etcpasswd.h"
+#include "etcshadow.h"
+#include <crypt.h> // 必須包含這個標頭檔
+
+Passwd passwd;   
+Shadow shadow;
+#define PASS 1
+#define FAIL 0
+#define ATTEMPT_COUNT 3
+
+int verify_password(const char *input_plain_password, const char *shadow_password_hash) {
+    // crypt 會回傳加密後的結果指標
+    char *hashed_input = crypt(input_plain_password, shadow_password_hash);
+
+    if (hashed_input == NULL) {
+        perror("crypt error");
+        return 0;
+    }
+
+    // 比較新產生的雜湊值與 Shadow 檔案中的雜湊值
+    if (strcmp(hashed_input, shadow_password_hash) == 0) {
+        return PASS; // 密碼正確
+    } else {
+        return FAIL; // 密碼錯誤
+    }
+}
+int auth(){
+	int attempt = 0;
+    char username[100];
+    char *password;
+
+    printf("Username: ");
+    scanf("%s", username);
+	// 1. check user exist
+    if (!read_passwd(username, &passwd) ||
+        !read_shadow(username, &shadow)){ 
+        printf("No such user\n");
+        return FAIL;
+    }
+
+	// 2. check user can access
+    if (strstr(passwd.shell, "nologin") ||
+        strstr(passwd.shell, "false") ||
+        strstr(passwd.shell, "sync") ) {
+        printf("This account can't be logged\n");
+        return FAIL;
+    }
+
+	// 3. check user password
+	getchar();
+	password = getpass("Password: ");
+	while (!verify_password(password, shadow.password) &&
+			attempt < ATTEMPT_COUNT) {
+        printf("Try again.\n");
+		password = getpass("Password: ");
+		attempt++;
+		}
+	if (attempt >= ATTEMPT_COUNT)
+        return FAIL;
+
+    return PASS;
+}
 int main(){
     pid_t pid;
     int status;
 
-    //getchar();
+    while (1){
+        if (auth() == FAIL) {
+            continue;}
+        printf("Loged\n");
 
-    pid = fork();
+        pid = fork();
 
-    // is child
-    if (pid == 0){
-        execlp("bash", "bash", NULL);
+        // is child
+        if (pid == 0){
+            setuid(passwd.uid);
+            printf("login with username:%s, uid:%d\n", passwd.username, passwd.uid);
+            execlp(passwd.shell, passwd.shell, NULL);
+        }
+        // is parent
+        else if (pid > 0){
+            wait(&status);
+            printf("Program quit\n");
+        }
+        else {
+            perror("error");
+            exit(EXIT_FAILURE);
+        }
     }
-    // is parent
-    else if (pid > 0){
-        wait(&status);
-        printf("Program quit\n");
-    }
-    else {
-        perror("error");
-        exit(EXIT_FAILURE);
-    }
-
 } 
